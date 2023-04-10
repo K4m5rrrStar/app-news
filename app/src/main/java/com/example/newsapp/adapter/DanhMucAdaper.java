0155e6bd9c5c29c85d8b7df67c64f70a60102778
K4m5rrrStar <nguyenvanmang2608@gmail.com>
2023-04-10 09:07:37 +0700

    update

---
+++ app/src/main/java/com/example/newsapp/adapter/DanhMucAdaper.java
@@ -0,0 +1,14 @@
+package com.example.newsapp.adapter;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+public class DanhMucAdaper {
+
+}

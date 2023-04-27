7a81b05c1b465008d25470d872a0dae5d8ebcfb3
K4m5rrrStar <nguyenvanmang2608@gmail.com>
2023-04-27 10:01:35 +0700

    update

--- app/src/main/java/com/example/newsapp/activities/BanTinActivity.java
+++ app/src/main/java/com/example/newsapp/activities/BanTinActivity.java
@@ -92,5 +92,26 @@ public class BanTinActivity extends AppCompatActivity {
             mLinTucAdaper.notifyDataSetChanged();
             super.onPostExecute(s);
         }
+
+        protected String getData(String theUrl) {
+            StringBuilder content = new StringBuilder();
+            try {
+                // create a url object
+                URL url = new URL(theUrl);
+                // create a urlconnection object
+                URLConnection urlConnection = url.openConnection();
+                // wrap the urlconnection in a bufferedreader
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                String line;
+                // read from the urlconnection via the bufferedreader
+                while ((line = bufferedReader.readLine()) != null) {
+                    content.append(line + "\n");
+                }
+                bufferedReader.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return content.toString();
+        }
     }
 }
